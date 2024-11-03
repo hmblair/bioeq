596119a94689fd10f0c99f4da9ebbbbdb75f2027
Hamish B <hmblair@stanford.edu>
2024-11-03 13:29:53 -0800

    Updated progress bar

--- bioeq/utils.py
+++ bioeq/utils.py
@@ -1,11 +1,17 @@
 from __future__ import annotations
+from typing import Iterator
 from torch.optim.lr_scheduler import _LRScheduler
 from torch.optim.optimizer import Optimizer
+from torch.utils.data import DataLoader
+import torch
+from tqdm import tqdm
+import wandb
 
 
 class LinearWarmupSqrtDecay(_LRScheduler):
     """
-    A learning rate scheduler with a linear warmup stage and then a square-root decay stage.
+    A learning rate scheduler with a linear warmup stage and then a square-root
+    decay stage.
     """
 
     def __init__(
@@ -51,3 +57,94 @@ class LinearWarmupSqrtDecay(_LRScheduler):
             group['lr'] * (scale ** (-1/2))
             for group in self.optimizer.param_groups
         ]
+
+
+class ProgressBar:
+    """
+    Dispaly the progress of the current epoch. Track the current and average
+    loss.
+    """
+
+    def __init__(
+        self: ProgressBar,
+        data: DataLoader,
+        name: str,
+    ) -> None:
+
+        # The name of this dataset
+        self.name = name
+        # The data we iterate over
+        self.data = data
+        # Keep track of the current epoch and step
+        self.curr_epoch = -1
+        self.curr_step = -1
+        # The current loss and a moving average
+        self.loss = 0
+        self.av_loss = 0
+        # Initialise the progress bar
+        self.pbar = None
+
+    def pbar_str(
+        self: ProgressBar,
+    ) -> str:
+        """
+        The progress bar description.
+        """
+
+        return f'Epoch {self.curr_epoch}; loss {self.loss:.2E}; average_loss {self.av_loss:.2E}'
+
+    def update(
+        self: ProgressBar,
+        loss: torch.Tensor | None = None,
+    ) -> None:
+        """
+        Increment the step. Update the loss and the average loss if a
+        loss is provided.
+        """
+
+        if self.pbar is None:
+            raise RuntimeError(
+                "The epoch method must be called before"
+                " updating the progress bar."
+            )
+
+        self.curr_step += 1
+        if loss is not None:
+            self.loss = loss
+            self.av_loss = (
+                self.curr_step * self.av_loss + self.loss
+            ) / (self.curr_step + 1)
+            self.pbar.set_description(self.pbar_str())
+            wandb.log(
+                {f'{self.name} loss': self.loss}
+            )
+
+    def epoch(
+        self: ProgressBar,
+    ) -> None:
+        """
+        Increment the epoch counter and reset the step counter and average loss.
+        """
+
+        # Create a new progress bar
+        self.pbar = tqdm(
+            self.data,
+            desc=self.pbar_str()
+        )
+
+        self.curr_epoch += 1
+        self.curr_step = -1
+        self.av_loss = 0
+
+    def __iter__(
+        self: ProgressBar,
+    ) -> Iterator:
+        """
+        Iterate over the dataloader.
+        """
+
+        if self.pbar is None:
+            raise RuntimeError(
+                "The epoch method must be called before iteration."
+            )
+        return self.pbar.__iter__()
